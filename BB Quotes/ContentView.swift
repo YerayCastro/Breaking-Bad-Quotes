95783a8fe0eee4e9eb5dec99d2c3f301bdd4efa4
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-06-21 12:52:34 +0200

    commit

---
+++ BB Quotes/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  BB Quotes
+//
+//  Created by Yery Castro on 14/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            QuoteView(show: Constants.bbName)
+                .tabItem {
+                    Label(Constants.bbName, systemImage: "tortoise")
+                }
+            
+            QuoteView(show: Constants.bcsName)
+                .tabItem {
+                    Label(Constants.bcsName, systemImage: "briefcase")
+                }
+        }
+        .onAppear {
+            UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BB Quotes/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  BB Quotes
+//
+//  Created by Yery Castro on 14/6/23.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+    let production: String
+}
